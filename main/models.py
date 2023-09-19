258f4f8a0aa8792375fc26b9850857ad5d5f4e95
Adrian <adrian.hamzah15@gmail.com>
2023-09-19 23:44:43 +0700

    new property added and models fixed

--- main/models.py
+++ main/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
+    price = models.IntegerField()
     amount = models.IntegerField()
     description = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
